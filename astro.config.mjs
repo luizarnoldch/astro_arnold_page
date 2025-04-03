df27595d1c6ec4c36dfc6378b93a3f69cfac2a4c
arnold <luizarnoldch@gmail.com>
2025-04-03 12:45:17 -0500

    format the aslias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
+// astro.config.mjs
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
+import react from "@astrojs/react"
+import { fileURLToPath } from "node:url"
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
   },
-
   integrations: [react()],
 })
